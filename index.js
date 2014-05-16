5f640d76f35c63fb339b53545e771ceb6ed5f28e
davidmarkclements <huperekchuno@googlemail.com>
2014-05-16 23:10:20 +0100

    restructuring

--- index.js
+++ index.js
@@ -2,31 +2,62 @@
 "use strict";
 
 var _     = require('underscore')
-var nid   = require('nid')
+var path = require('path')
+var crypto = require('crypto');
+var fs = require('fs');
+
 
 var node = require('./lib/node')
 var ping = require('./lib/ping')
 
+var state = {
+  nodes: [],
+  pings: {},
+}
+
+function syncHashFile(filePath, salt) {
+  salt = salt || '';
+  syncHashFile[filePath] = fs.readFileSync(filePath);
+  return crypto.createHash('md5').update(syncHashFile[filePath], salt).digest('hex');
+}
+
+
 module.exports = function(options) {
   var seneca = this
   var name = 'heart'
+  var mainModule = process.mainModule.filename
 
   options = seneca.util.deepextend({
-    id: nid(),
+    id: syncHashFile(mainModule),
     monitor: false,
-    minwait: 1111,
-    maxwait: 2222,
-    maxerr: 11,
-    culltime: (11 * 60 * 1000)
-  }, options)
+    persist: {
+      plugin: 'jsonfile-store'
+    }
+  }, options);
 
-
-  var state = {
-    clients: {},
-    actives: [],
-    nodes: [],
-    pings: {}
+  if (options.persist.plugin === 'jsonfile-store' && !options.persist.options) {
+    options.persist.options = {folder: path.join(__dirname, 'store')};
   }
+
+  if (options.monitor) {
+    options.monitor = seneca.util.deepextend({
+      interval: 1111,
+      timeout: 2222,
+      attempts: 11,
+      cull: 66e4, //11 minutes
+    }, _.isObject(options.monitor) ? options.monitor : {});
+  }
+
+  if (options.id in state.nodes) {
+    state.nodes[options.id].cousins = state.nodes[options.id].cousins || [];
+    options.id = syncHashFile(mainModule, state.nodes[options.id].cousins.length);
+    state.nodes[options.id].cousins.push(options.id);
+  }
+
+  console.log(options);
+
+  seneca.use(options.persist.plugin, options.persist.options)
+
 
   ping = ping(seneca, state, options, function () {return node;});
   node = node(seneca, state, options, function () {return ping;});
@@ -34,23 +65,17 @@ module.exports = function(options) {
   function status(args, done) {
     node.entity.list$(function(err,list){
       if (err) { return done(err) };
-
       done(null, {
-        actives: _.clone(actives),
-        pings: _.clone(pings),
+        pingers: Object.keys(pings).length,
         nodes: nodes,
+        reachable: nodes.map(function(n){return n.active;}),
+        unreachable: nodes.map(function(n){return !n.active;}),
         list: list
       })
-
     })
-
   }
   
   // actions provided
-  seneca.add({role:name, cmd:'send-ping'}, ping.send)
-
-  seneca.add({role:name, cmd:'answer-ping'}, ping.answer)
-
   seneca.add({role:name, cmd:'add-node'}, node.add)
 
   seneca.add({role:name, cmd:'remove-node'}, node.remove)
@@ -59,14 +84,13 @@ module.exports = function(options) {
 
   seneca.add({role:name, cmd:'status'}, status)
 
-  // resolve entity args by id
-  seneca.act({
-    role:   'util',
-    cmd:    'ensure_entity',
-    pin:    { role:name, cmd:'send-ping' },
-    entmap: { node: node.entity }
-  });
 
+  seneca.act({role:name, cmd: 'add-node', id: options.id, path: mainModule})
+
+  if (options.monitor) {
+    ping.all();
+  }
+  
 
   return {
     name: name
